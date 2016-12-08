94287d0252cbacd5d0bfcdd4a332acbe508b4fc5
WeiFeng <weifeng@weidian.com>
2016-12-08 17:42:08 +0800

    加入Adapter中的布局

--- app/src/main/java/co/songlcy/recyclerviewanimation/adapter/RecyclerViewAdapter.java
+++ app/src/main/java/co/songlcy/recyclerviewanimation/adapter/RecyclerViewAdapter.java
@@ -6,52 +6,69 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 import co.songlcy.recyclerviewanimation.R;
 
 /**
  * Created by Song on 2016/7/19.
  */
-public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.BaseViewHolder>{
+public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapter.BaseViewHolder> {
 
     private int type = 0;
     private String[] strDescs;
     private String[] strItems;
     private Context mContext;
     private LayoutInflater inflater;
+    private static List<Integer> mTipIds = new ArrayList<>(Arrays.asList(R.id.tip1, R.id.tip2, R.id.tip3, R.id.tip4, R.id.tip5, R.id.tip6, R.id.tip7, R.id.tip8));
+    private static String[] tipDatas;
+
     public RecyclerViewAdapter(Context context, String[] strItems, String[] descs) {
 
         this.strItems = strItems;
         this.strDescs = descs;
         this.mContext = context;
         inflater = LayoutInflater.from(context);
+        tipDatas = context.getResources().getStringArray(R.array.str_tips);
     }
 
     @Override
     public BaseViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
 
         View rootView;
-        if(viewType == 0) {
-            rootView = inflater.inflate(R.layout.layout_linear,null,false);
+        if (viewType == 0) {
+            rootView = inflater.inflate(R.layout.layout_linear, null, false);
             LinearViewHolder linearViewHolder = new LinearViewHolder(rootView);
             return linearViewHolder;
-        } else {
-
-            rootView = inflater.inflate(R.layout.layout_grid,null,false);
+        } else if (viewType == 1) {
+            rootView = inflater.inflate(R.layout.layout_grid, null, false);
             GridViewHolder gridViewHolder = new GridViewHolder(rootView);
             return gridViewHolder;
+        } else {
+            rootView = inflater.inflate(R.layout.layout_grid_tips, null, false);
+            LineLayoutTipsHolder lineLayoutTipsHolder = new LineLayoutTipsHolder(rootView);
+            return lineLayoutTipsHolder;
         }
     }
 
     @Override
     public void onBindViewHolder(BaseViewHolder holder, int position) {
 
-        if(type == 0) {
-            LinearViewHolder linearViewHolder = (LinearViewHolder)holder;
+        if (type == 0&&position!=8) {
+            LinearViewHolder linearViewHolder = (LinearViewHolder) holder;
             linearViewHolder.tvName.setText(strItems[position]);
             linearViewHolder.tvDesc.setText(strDescs[position]);
-        } else {
-            GridViewHolder gridViewHolder = (GridViewHolder)holder;
+        } else if (type == 1&&position!=8) {
+            GridViewHolder gridViewHolder = (GridViewHolder) holder;
             gridViewHolder.tvName.setText(strItems[position]);
+        } else if (type == 8) {
+            LineLayoutTipsHolder tipsHolder = (LineLayoutTipsHolder) holder;
+            for (int i = 0; i < 8; i++) {
+                tipsHolder.tvNames.get(i).setText(tipDatas[i]);
+            }
         }
     }
 
@@ -62,14 +79,18 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
     @Override
     public int getItemViewType(int position) {
-        return type;
+        if(position == 8){
+            return 2;
+        }else {
+            return type;
+        }
     }
 
     public void setType(int type) {
         this.type = type;
     }
 
-    public static class BaseViewHolder extends RecyclerView.ViewHolder{
+    public static class BaseViewHolder extends RecyclerView.ViewHolder {
 
         public BaseViewHolder(View itemView) {
             super(itemView);
@@ -95,6 +116,20 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
         public GridViewHolder(View itemView) {
             super(itemView);
             tvName = (TextView) itemView.findViewById(R.id.tv_name);
+        }
+    }
+
+    public static class LineLayoutTipsHolder extends BaseViewHolder {
+
+        private List<TextView> tvNames = new ArrayList<TextView>();
+        private TextView tvName;
+
+        public LineLayoutTipsHolder(View itemView) {
+            super(itemView);
+            for (int i = 0; i < tipDatas.length; i++) {
+                tvName = (TextView) itemView.findViewById(mTipIds.get(i));
+                tvNames.add(tvName);
+            }
         }
     }
 }
